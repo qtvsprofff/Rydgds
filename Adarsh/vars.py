25df22c3570de2f8dd4dbfee05a044ebab99e1ac
qtvsprofff <142015253+qtvsprofff@users.noreply.github.com>
2023-08-18 10:29:01 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -9,21 +9,21 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '14505719'))
-    API_HASH = str(getenv('API_HASH', '620f0a2aa2cd1474a4953619b3e3643d'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6126205588:AAEpoJttxR56CI9ubvy7P6O89ENjD7aGqWU'))
+    API_ID = int(getenv('API_ID', '11427887'))
+    API_HASH = str(getenv('API_HASH', '317ea7bfc3dcbfb2a4aa7c866110074e'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6486983020:AAHLVzW2TAnlqXVZmJofY8wuvqTkv4zwm3M'))
     SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkda'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001659362808'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001854186393'))
     PORT = int(getenv('PORT', 8080))
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1893496887").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1044163529").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    SHORTENER_API = str(getenv('SHORTENER_API', '4fbe384f585aa6c0df736bd9a859bfc3b9216fee'))
-    SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnlink.in'))
+    SHORTENER_API = str(getenv('SHORTENER_API', '02c93c55c4567035a37ffc32731d8f0e1c530f98'))
+    SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnshort.net'))
     FDQN = str(getenv('FDQN', 'https://filetolinkda.onrender.com'))
 
     APP_NAME = None
@@ -40,6 +40,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://Haashim:Haashim@mfile0.t9hxg.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@tamilanmovieschat"))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://hillsking1222:kuraman1@kumaran1.d6ahc05.mongodb.net/?retryWrites=true&w=majority"))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@Qtmve_linkzZz"))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001666069456")).split())) 
